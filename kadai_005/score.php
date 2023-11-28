50c9a2bb8177b61b56ad38b1c2c724482046e146
bocroa0911 <146640088+bocroa0911@users.noreply.github.com>
2023-11-28 20:43:26 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_005</title>
+</head>
+
+<body>
+    <p>
+    
+
+        <?php
+
+        // 変数の代入
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        // 合計点
+        $total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        echo '合計点 : ';
+        echo $total_score;
+
+        echo '<br>';
+
+        // 平均点
+        $average_score = $total_score / 10;
+        echo '平均点 : ';
+        echo $average_score;
+
+        ?>
+    </p>
+</body>
+
+</html>    
